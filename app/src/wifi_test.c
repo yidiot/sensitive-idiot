5d559f48781d32638ac17cb00200fd4dace284b1
Gaël PORTAY <gael.portay@rtone.fr>
2024-12-30 20:44:59 +0100

    Autoconnect to Wi-FI

--- app/src/wifi_test.c
+++ app/src/wifi_test.c
@@ -1,14 +1,15 @@
 /*
  * Copyright (c) 2024 Gaël PORTAY
- *               2016 Intel Corporation.
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #include <zephyr/kernel.h>
+#include <zephyr/net/net_if.h>
+#include <zephyr/net/wifi_mgmt.h>
 
 int main(void)
 {
-
-	return 0;
+	struct net_if *iface = net_if_get_first_by_type(&NET_L2_GET_NAME(ETHERNET));
+	return net_mgmt(NET_REQUEST_WIFI_CONNECT_STORED, iface, NULL, 0);
 }
